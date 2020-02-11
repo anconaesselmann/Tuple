57da0aff70c2730f651de756c5fe08c23f84c86c
Axel Ancona Esselmann <axel@vida.com>
2020-02-11 02:22:56 -0800

    Example playground

---
+++ Example.playground/Contents.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+// 1) Setup:
+// Lets create some players and shuffle a deck of cards.
+
+// This game of cat and mouse and dog is played with three player.
+let allPlayers = (cat: "Tom", mouse: "Jerry", dog: "Pluto")
+
+enum Suit: String, CaseIterable { case clubs; case diamonds; case hearts; case spades }
+
+// Let's generate a shuffled array of tuples to represent our deck of cards:
+var deck: [(suit: Suit, value: Int)] = Suit.allCases.flatMap {
+    Array(zip(Array(repeating: $0, count: 13), Array(1...13)))
+}.shuffled()
+
+//2) Let's play! Here we finally get to use the Tuple dependency.
+
+// We keep track of all played cards in an array of tripples that includes the player wo drew the card:
+var playedCards: [(round: Int, suit: Suit, value: Int, player: String)] = []
+
+var round = 0
+while !deck.isEmpty {
+    // Each round is played with only two players that are chosen at random
+
+    // First we convert the tripple of all players into an array and shuffle it:
+    let shuffled = Tuple.arrayFromTuple(allPlayers).shuffled()
+
+    // Now lets select two players and use a tuple to represent the players of this round:
+    let opponents: (firstTurn: String, secondTurn: String) = Tuple.tuple(from: Array(shuffled[0...1]))!
+
+    // We now convert the tuple of opponents into an array so we can itterate over it.
+    for currentPlayer in Tuple.arrayFromTuple(opponents) {
+
+        // let's pick a card and add it to our array of played cards.
+        let card = deck.popLast()!
+
+        // We first have to turn the card tuple into a played card triple:
+        let playedCard: (suit: Suit, value: Int, player: String) = Tuple.append(value: currentPlayer, toTuple: card)
+
+        // We will need to the round to calculate the final scores. Let's prepend it to the playedCard tuple. Because we can ;-)
+        let playedCardInRound: (round: Int, suit: Suit, value: Int, player: String) = Tuple.prepend(value: round, toTuple: playedCard)
+
+        // Now we are able to add the card to our pile of playedCards
+        playedCards.append(playedCardInRound)
+    }
+    round += 1
+}
+
+// 3) Who won? For good measure let's use some more Tuple methods.
+
+// Let's calculate scores based on the players target suit, the value of their card and the round number
+func calculateValue(player: String, suit: Suit) -> Int {
+    return playedCards
+        .filter { $0.player == player }
+        .filter { $0.suit == suit }
+        .reduce(into: 0) { $0 += $1.round - $1.value }
+}
+
+let scores = (
+    catScore: calculateValue(player: allPlayers.cat, suit: .hearts),
+    mouseScore: calculateValue(player: allPlayers.mouse, suit: .diamonds),
+    dogScore: calculateValue(player: allPlayers.dog, suit: .clubs)
+)
+
+// We determine the actual winner by first picking one of the scores at random.
+
+let tupleOrder = Tuple.order(of: scores)
+var elementIndices: [Int] = Array(0..<tupleOrder).shuffled()
+let elementIndex = elementIndices.popLast()!
+let targetScore: Int = Tuple.element(at: elementIndex, in: scores)!
+
+// If the picked score is a duplicate, everyone with the same score wins
+// TODO: implement
+
+// If the picked score is the highest score, both lower scores get a point, and if the picked score is the lowest score, both higher scores get a point
+// TODO: implement
+
+// If the picked score is in the middle, the picked score gets a point.
+// TODO: implement
+
---
+++ Example.playground/Sources/Tuple.swift
@@ -0,0 +1,392 @@
+//  Created by Axel Ancona Esselmann on 2/10/20.
+//  Copyright © 2020 Axel Ancona Esselmann. All rights reserved.
+//
+
+import Foundation
+
+public struct Tuple {
+
+    static public func toTuple<Value, T1>(_ value: Value, tuple: T1) -> (Value, T1) {
+        return (value, tuple)
+    }
+
+    // MARK: - prepend
+
+    static public func prepend<Value, T1>(value: Value, toTuple tuple: (T1)) -> (Value, T1) {
+        return toTuple(value, tuple: tuple)
+    }
+
+    static public func prepend<Value, T1, T2>(value: Value, toTuple tuple: (T1, T2)) -> (Value, T1, T2) {
+        return (value, tuple.0, tuple.1)
+    }
+
+    static public func prepend<Value, T1, T2, T3>(value: Value, toTuple tuple: (T1, T2, T3)) -> (Value, T1, T2, T3) {
+        return (value, tuple.0, tuple.1, tuple.2)
+    }
+
+    static public func prepend<Value, T1, T2, T3, T4>(value: Value, toTuple tuple: (T1, T2, T3, T4)) -> (Value, T1, T2, T3, T4) {
+        return (value, tuple.0, tuple.1, tuple.2, tuple.3)
+    }
+
+    static public func prepend<Value, T1, T2, T3, T4, T5>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5)) -> (Value, T1, T2, T3, T4, T5) {
+        return (value, tuple.0, tuple.1, tuple.2, tuple.3, tuple.4)
+    }
+
+    static public func prepend<Value, T1, T2, T3, T4, T5, T6>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6)) -> (Value, T1, T2, T3, T4, T5, T6) {
+        return (value, tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5)
+    }
+
+    static public func prepend<Value, T1, T2, T3, T4, T5, T6, T7>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6, T7)) -> (Value, T1, T2, T3, T4, T5, T6, T7) {
+        return (value, tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6)
+    }
+
+    static public func prepend<Value, T1, T2, T3, T4, T5, T6, T7, T8>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6, T7, T8)) -> (Value, T1, T2, T3, T4, T5, T6, T7, T8) {
+        return (value, tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7)
+    }
+
+    // MARK: - append
+
+    static public func append<T1, Value>(value: Value, toTuple tuple: (T1)) -> (T1, Value) {
+        return toTuple(tuple, tuple: value)
+    }
+
+    static public func append<T1, T2, Value>(value: Value, toTuple tuple: (T1, T2)) -> (T1, T2, Value) {
+        return (tuple.0, tuple.1, value)
+    }
+
+    static public func append<T1, T2, T3, Value>(value: Value, toTuple tuple: (T1, T2, T3)) -> (T1, T2, T3, Value) {
+        return (tuple.0, tuple.1, tuple.2, value)
+    }
+
+    static public func append<T1, T2, T3, T4, Value>(value: Value, toTuple tuple: (T1, T2, T3, T4)) -> (T1, T2, T3, T4, Value) {
+        return (tuple.0, tuple.1, tuple.2, tuple.3, value)
+    }
+
+    static public func append<T1, T2, T3, T4, T5, Value>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5)) -> (T1, T2, T3, T4, T5, Value) {
+        return (tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, value)
+    }
+
+    static public func append<T1, T2, T3, T4, T5, T6, Value>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6)) -> (T1, T2, T3, T4, T5, T6, Value) {
+        return (tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, value)
+    }
+
+    static public func append<T1, T2, T3, T4, T5, T6, T7, Value>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6, T7)) -> (T1, T2, T3, T4, T5, T6, T7, Value) {
+        return (tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, value)
+    }
+
+    static public func append<T1, T2, T3, T4, T5, T6, T7, T8, Value>(value: Value, toTuple tuple: (T1, T2, T3, T4, T5, T6, T7, T8)) -> (T1, T2, T3, T4, T5, T6, T7, T8, Value) {
+        return (tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, value)
+    }
+
+    // MARK: - arrayFromTuple
+
+    static public func arrayFromTuple<T>(_ tuple: T) -> Array<T> {
+        return [
+            tuple
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2,
+            tuple.3
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2,
+            tuple.3,
+            tuple.4
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T, T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2,
+            tuple.3,
+            tuple.4,
+            tuple.5
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T, T, T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2,
+            tuple.3,
+            tuple.4,
+            tuple.6
+        ]
+    }
+
+    static public func arrayFromTuple<T>(_ tuple: (T, T, T, T, T, T, T, T)) -> Array<T> {
+        return [
+            tuple.0,
+            tuple.1,
+            tuple.2,
+            tuple.3,
+            tuple.4,
+            tuple.6,
+            tuple.7
+        ]
+    }
+
+    // MARK: - tuple(from: [T])
+
+    static public func tuple<T>(from array: [T]) -> (T, T)? {
+        guard array.count == 2 else {
+            return nil
+        }
+        return (array[0], array[1])
+    }
+
+    static public func threeTuple<T>(from array: [T]) -> (T, T, T)? {
+        guard array.count == 3 else {
+            return nil
+        }
+        return (array[0], array[1], array[2])
+    }
+
+    static public func triple<T>(from array: [T]) -> (T, T, T)? {
+        return threeTuple(from: array)
+    }
+
+    static public func fourTuple<T>(from array: [T]) -> (T, T, T, T)? {
+        guard array.count == 4 else {
+            return nil
+        }
+        return (array[0], array[1], array[2], array[3])
+    }
+
+    static public func quadruple<T>(from array: [T]) -> (T, T, T, T)? {
+        return fourTuple(from: array)
+    }
+
+    static public func fiveTuple<T>(from array: [T]) -> (T, T, T, T, T)? {
+        guard array.count == 5 else {
+            return nil
+        }
+        return (array[0], array[1], array[2], array[3], array[4])
+    }
+
+    static public func quintuple<T>(from array: [T]) -> (T, T, T, T, T)? {
+        return fiveTuple(from: array)
+    }
+
+    static public func sixTuple<T>(from array: [T]) -> (T, T, T, T, T, T)? {
+        guard array.count == 6 else {
+            return nil
+        }
+        return (array[0], array[1], array[2], array[3], array[4], array[5])
+    }
+
+    static public func sextuple<T>(from array: [T]) -> (T, T, T, T, T, T)? {
+        return sixTuple(from: array)
+    }
+
+    static public func sevenTuple<T>(from array: [T]) -> (T, T, T, T, T, T, T)? {
+        guard array.count == 7 else {
+            return nil
+        }
+        return (array[0], array[1], array[2], array[3], array[4], array[5], array[6])
+    }
+
+    static public func septuple<T>(from array: [T]) -> (T, T, T, T, T, T, T)? {
+        return sevenTuple(from: array)
+    }
+
+    static public func eightTuple<T>(from array: [T]) -> (T, T, T, T, T, T, T, T)? {
+        guard array.count == 8 else {
+            return nil
+        }
+        return (array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7])
+    }
+
+    static public func octuple<T>(from array: [T]) -> (T, T, T, T, T, T, T, T)? {
+        return eightTuple(from: array)
+    }
+
+    // MARK: - tuple order
+
+    static public func order<T1, T2>(of tuple: (T1, T2)) -> Int {
+        return 2
+    }
+
+    static public func order<T1, T2, T3>(of tuple: (T1, T2, T3)) -> Int {
+        return 3
+    }
+
+    static public func order<T1, T2, T3, T4>(of tuple: (T1, T2, T3, T4)) -> Int {
+        return 4
+    }
+
+    static public func order<T1, T2, T3, T4, T5>(of tuple: (T1, T2, T3, T4, T5)) -> Int {
+        return 5
+    }
+
+    static public func order<T1, T2, T3, T4, T5, T6>(of tuple: (T1, T2, T3, T4, T5, T6)) -> Int {
+        return 6
+    }
+
+    static public func order<T1, T2, T3, T4, T5, T6, T7>(of tuple: (T1, T2, T3, T4, T5, T6, T7)) -> Int {
+        return 7
+    }
+
+    static public func order<T1, T2, T3, T4, T5, T6, T7, T8>(of tuple: (T1, T2, T3, T4, T5, T6, T7, T8)) -> Int {
+        return 8
+    }
+
+    // MARK: - nilValues
+
+    static public func nilValues<T1>() -> T1? {
+        return nil
+    }
+
+    static public func nilValues<T1, T2>() -> (T1?, T2?) {
+        return (nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3>() -> (T1?, T2?, T3?) {
+        return (nil, nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3, T4>() -> (T1?, T2?, T3?, T4?) {
+        return (nil, nil, nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3, T4, T5>() -> (T1?, T2?, T3?, T4?, T5?) {
+        return (nil, nil, nil, nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3, T4, T5, T6>() -> (T1?, T2?, T3?, T4?, T5?, T6?) {
+        return (nil, nil, nil, nil, nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3, T4, T5, T6, T7>() -> (T1?, T2?, T3?, T4?, T5?, T6?, T7?) {
+        return (nil, nil, nil, nil, nil, nil, nil)
+    }
+
+    static public func nilValues<T1, T2, T3, T4, T5, T6, T7, T8>() -> (T1?, T2?, T3?, T4?, T5?, T6?, T7?, T8?) {
+        return (nil, nil, nil, nil, nil, nil, nil, nil)
+    }
+
+    // MARK: - element(at:in)
+
+    static public func element<T1, Element>(at index: Int, in tuple: T1) -> Element? {
+        switch index {
+            case 0: return tuple as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, Element>(at index: Int, in tuple: (T1, T2)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, Element>(at index: Int, in tuple: (T1, T2, T3)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, T4, Element>(at index: Int, in tuple: (T1, T2, T3, T4)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+            case 3: return tuple.3 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, T4, T5, Element>(at index: Int, in tuple: (T1, T2, T3, T4, T5)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+            case 3: return tuple.3 as? Element
+            case 4: return tuple.4 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, T4, T5, T6, Element>(at index: Int, in tuple: (T1, T2, T3, T4, T5, T6)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+            case 3: return tuple.3 as? Element
+            case 4: return tuple.4 as? Element
+            case 5: return tuple.5 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, T4, T5, T6, T7, Element>(at index: Int, in tuple: (T1, T2, T3, T4, T5, T6, T7)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+            case 3: return tuple.3 as? Element
+            case 4: return tuple.4 as? Element
+            case 5: return tuple.5 as? Element
+            case 6: return tuple.6 as? Element
+        default:
+            return nil
+        }
+    }
+
+    static public func element<T1, T2, T3, T4, T5, T6, T7, T8, Element>(at index: Int, in tuple: (T1, T2, T3, T4, T5, T6, T7, T8)) -> Element? {
+        switch index {
+            case 0: return tuple.0 as? Element
+            case 1: return tuple.1 as? Element
+            case 2: return tuple.2 as? Element
+            case 3: return tuple.3 as? Element
+            case 4: return tuple.4 as? Element
+            case 5: return tuple.5 as? Element
+            case 6: return tuple.6 as? Element
+            case 7: return tuple.7 as? Element
+        default:
+            return nil
+        }
+    }
+
+}
+
+
